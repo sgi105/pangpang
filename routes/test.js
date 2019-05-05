31ec26cbc88fe7bca3382b7cbaf208acdd71f6a8
ruddls00114 <kkmm114@gmail.com>
2019-05-05 23:07:55 +0900

    my commit

--- routes/test.js
+++ routes/test.js
@@ -1,15 +1,8 @@
 const mysql = require('mysql');
 const express = require('express');
 const router = express.Router();
-// const dbConfig = {
-// 	host : 'localhost', //연결할 대상의 IP or DNS 주소
-// 	port : '3306', //Port번호, 주로 MySQL의 Default인 3306을 쓴다
-// 	user : 'root', //연결할 대상의 계정
-// 	password : 'poiu0987', //비밀번호
-// 	database : 'sopt21th' //접속할 schema 이름
-// };
 const dbConfig = require('../config/dbPool')
-
+const resMsg = require('../module/responseMessage')
 //테이블에 저장된 모든 영화정보를 조회
 router.get('/', (req, res)=> {
 	let connection = mysql.createConnection(dbConfig); //
@@ -18,7 +11,7 @@ router.get('/', (req, res)=> {
 		if(err){
 			res.status(500).send({
 				stat : "fail",
-				msg : "loading movies data fail"
+				msg : resMsg.NULL_VALUE
 			});
 			connection.end();
 			console.log("select all query error : "+ err);
@@ -30,7 +23,7 @@ router.get('/', (req, res)=> {
 				msg : "successful loading movies data"
 			});
 			connection.end();
-			console.log("successful loading movies data");
+			console.log(resMsg.NULL_VALUE);
 		}
 	});
 });
