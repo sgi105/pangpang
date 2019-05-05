d2b7bd312704000f89788d86c073fcd92784860b
Ga In Shin <sgi105@github.com>
2019-05-05 22:30:57 +0900

    asdf

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-const user = require('./users')
-const mission =require('./mission')
-const message =require('./message')
+const user = require("./users");
+const mission = require("./mission");
+const message = require("./message");
+const test = require("./test");
 
-router.use('/users', user);
-router.use('/mission', mission);
-router.use('/message', message);
-module.exports = router
+// router.use('/users', user);
+router.use("/mission", mission);
+router.use("/message", message);
+router.use("/test", test);
+
+module.exports = router;
