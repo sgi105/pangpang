71ada348583606cd56781be3563ef19e59f69e02
ruddls00114 <kkmm114@gmail.com>
2019-05-06 02:38:33 +0900

    message ADD

--- routes/message/index.js
+++ routes/message/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+
+const message = require('./message.js')
+
+
+router.use('/',message)
+
 module.exports = router;
--- routes/test.js
+++ routes/test.js
@@ -1,33 +1,6 @@
 const mysql = require("mysql");
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const dbConfig = require('../config/dbPool')
-const resMsg = require('../module/responseMessage')
-//테이블에 저장된 모든 영화정보를 조회
-router.get('/', (req, res)=> {
-	let connection = mysql.createConnection(dbConfig); //
-	let selectAllQuery = 'select * from parents';
-	connection.query(selectAllQuery, (err, result) => {
-		if(err){
-			res.status(500).send({
-				stat : "fail",
-				msg : resMsg.NULL_VALUE
-			});
-			connection.end();
-			console.log("select all query error : "+ err);
-		}
-		else{
-			res.status(200).send({
-				stat : "success",
-				data : result,
-				msg : "successful loading movies data"
-			});
-			connection.end();
-			console.log(resMsg.NULL_VALUE);
-		}
-	});
-=======
 const dbConfig = require("../config/dbPool");
 
 //테이블에 저장된 모든 영화정보를 조회
@@ -52,7 +25,6 @@ router.get("/", (req, res) => {
       console.log("successful loading movies data");
     }
   });
->>>>>>> bdad7cacfcc271b53dd816a4f43fc163373b617d
 });
 
 module.exports = router;
