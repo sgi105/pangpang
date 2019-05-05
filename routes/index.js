ed1d88d1d53be5059adac5738d9eaf748cdbed70
ruddls00114 <kkmm114@gmail.com>
2019-05-05 22:26:34 +0900

    edit folder

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const router = express.Router()
 
-const user = require('./users')
-const mission =require('./mission')
-const message =require('./message')
+// const user = require('./users')
+// const mission =require('./mission')
+// const message =require('./message')
+// const login = require('./users')
 
-router.use('/users', user);
-router.use('/mission', mission);
-router.use('/message', message);
+// router.post('/users', user);
+// router.use('/mission', mission);
+// router.use('/message', message);
+
+// router.use('/', login)
+router.use('/',require('./test.js'))
 module.exports = router
