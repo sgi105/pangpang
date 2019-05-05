bdad7cacfcc271b53dd816a4f43fc163373b617d
Ga In Shin <sgi105@github.com>
2019-05-05 23:08:12 +0900

    setting complete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const router = express.Router();
+const mission = require("./mission");
+const message = require("./message");
 
 // const user = require('./users')
 // const mission =require('./mission')
@@ -12,5 +14,7 @@ const router = express.Router();
 // router.use('/message', message);
 
 // router.use('/', login)
-router.use("/", require("./test.js"));
+router.use("/mission", mission);
+router.use("/message", message);
+router.use("/", require("./test"));
 module.exports = router;
